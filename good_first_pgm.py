347332703f0c9cd0f5255ae8691d10179e837ce0
jishnu <jishnurajesh555@gmail.com>
2018-08-05 22:42:12 +0530

    adding example to pushed file

--- good_first_pgm.py
+++ good_first_pgm.py
@@ -1,4 +1,7 @@
 print("hi python")
 print("hello world!!")
 print("I like typing this.")
-
+print("This is fun.")
+print('yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
