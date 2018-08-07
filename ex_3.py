96e76119781695cb9d73c9fda572cc74f6edc6c7
jishnu <jishnurajesh555@gmail.com>
2018-08-07 18:07:11 +0530

    maths operations

---
+++ ex_3.py
@@ -0,0 +1,32 @@
+print("I will now count my chickens:")
+
+
+
+print("Hens :", 25+30/6)
+print("Roosters :", 100 - 25 * 3 % 4)
+
+print("Now i will count the eggs:")
+
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+
+print("It is true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+
+
+print("What is 3 + 2?", "it is ", 3 + 2)
+print("What is 5 - 7?", "it is ", 5 - 7)
+
+
+print("Oh, that's why it's false.")
+
+
+print("How about some more.")
+
+
+print("Is 5 > -2 is greater?",  5 > -2)
+print("Is 5 >= -2 is greater or equal?",  5 >= -2)
+print("Is 5 <= -2  less or equal?",  5 <= -2)
+
