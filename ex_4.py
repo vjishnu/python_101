0880df708f660b7c4d9550e2e63c74ad2e03fad7
jishnu <jishnurajesh555@gmail.com>
2018-08-07 18:20:18 +0530

    cars details

--- ex_4.py
+++ ex_4.py
@@ -1,11 +1,11 @@
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
+cars = 100 # An integer assignment
+space_in_a_car = 4.0 # An integer assignment
+drivers = 30 # An integer assignment
+passengers = 90 # An integer assignment
 cars_not_driven = cars - drivers
 cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+carpool_capacity = cars_driven * space_in_a_car # multiplication
+average_passengers_per_car = passengers / cars_driven # division
 
 
 
