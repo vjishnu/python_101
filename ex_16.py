e76fe1aa58202991c0f524b99473c8f311380e2b
jishnu <jishnurajesh555@gmail.com>
2018-08-08 16:19:21 +0530

    iteration through list

---
+++ ex_16.py
@@ -0,0 +1,17 @@
+#list of numbers
+
+numbers = [6,5,3,8,4,2,4,11] # a list with integers
+
+#variable to store the sum
+
+sum = 0
+
+#to iterate over list
+
+for i in numbers: # for loop
+    print("The elements are ",{i})
+    sum = sum + i
+    print("The sum is ",sum)
+
+
+
