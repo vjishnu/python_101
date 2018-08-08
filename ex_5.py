c6200cd3c6f72cd977695dd12c5c4fc136212d33
jishnu <jishnurajesh555@gmail.com>
2018-08-08 16:28:49 +0530

    print fun. with end

---
+++ ex_5.py
@@ -0,0 +1,26 @@
+print("Mary had a little lamb.")
+print("And everywhere that Mary went.")
+print("." * 10)  # it prints the dot(.) character 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that "end" parameter at the end. (end=' ')
+# try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+print('*' * 15)
+# using end with another character
+print(end1 + end2 + end3 + end4 + end5 + end6, end='\n')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
