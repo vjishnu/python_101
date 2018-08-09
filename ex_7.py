c519550050a07f447d26b9b17789913b363c1660
jishnu <jishnurajesh555@gmail.com>
2018-08-09 14:06:23 +0530

    newline_charcter

---
+++ ex_7.py
@@ -0,0 +1,13 @@
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # months seperated by new line character
+
+print(" The days are : ", days) # print days
+print(" The months are : \n", months) #print months
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""") # within three double quotes all the statements are printed  
