6b7cf7137a835ff3c010ed925fee6826e2252871
jishnu <jishnurajesh555@gmail.com>
2018-08-09 14:41:42 +0530

    sys.argv

---
+++ ex_12.py
@@ -0,0 +1,16 @@
+import sys 
+prgrm_name = sys.argv[0]
+
+
+arguments = sys.argv[1:]
+count = len(arguments)
+
+from sys import argv
+script,first,second,third=argv
+
+print("The script is called : " ,script)
+print("The first variable is called : " ,first)
+print("The second variable is called : " ,second)
+print("The thrid variable is called : " ,third)
+
+
