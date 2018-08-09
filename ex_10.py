8bbc49580cc6ee0856073dfe3b5a4ca18db81914
jishnu <jishnurajesh555@gmail.com>
2018-08-09 14:30:51 +0530

    if statement

---
+++ ex_10.py
@@ -0,0 +1,27 @@
+people = 20 # initializing
+cats = 30
+dogs = 15
+
+
+if people < cats: # if statement with lessthan operator
+    print("Too many cats! The world is doomed!")
+
+if people > cats:  # if statement with greaterthan operator
+
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5 # dog = dog + 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
