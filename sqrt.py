5701163b21cb9bb9093706cbc894d35bd30d5e76
jishnu <jishnurajesh555@gmail.com>
2018-08-09 18:28:06 +0530

    sqrt_of_a_nmbr

---
+++ sqrt.py
@@ -0,0 +1,15 @@
+
+# to find square root without import math 
+
+num= int(input("enter the number"))
+sqrt = num ** (1.0/2)
+print("The square of given number is {}".format(sqrt)) 
+
+
+# to find square root using import math
+
+import math
+num= int(input("enter the number"))
+print("The square of given number is {}".format(math.sqrt(num)))
+
+
