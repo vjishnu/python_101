6ddffb26dee0baa4dbb7fe493220c287defd0e52
jishnu <jishnurajesh555@gmail.com>
2018-08-09 14:24:45 +0530

    using \t and \n

---
+++ ex_9.py
@@ -0,0 +1,16 @@
+# using \t character
+tabby_cat = "\tI'm tabbed in." # \t tabspace
+persian_cat = "I'm split\non a line." # \n new line
+backslash_cat = "I'm \\ a \\ cat." 
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
