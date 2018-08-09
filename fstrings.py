3f24886393a8df572873c9a6015e61db24de9ea4
jishnu <jishnurajesh555@gmail.com>
2018-08-09 21:09:54 +0530

    using_f strings

---
+++ fstrings.py
@@ -0,0 +1,19 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_age} inches tall.")
+print(f"He's {my_height} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"Let's talk about {my_name}.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee. If I add {my_age} , {my_height} and {my_weight} i get 289.")
+
+
+
+
