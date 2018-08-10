36c3e607dc93d205a898e3e0e7596ee885e00375
jishnu <jishnurajesh555@gmail.com>
2018-08-10 22:08:16 +0530

    update milestone_p_1

--- milestone_project_1.py
+++ milestone_project_1.py
@@ -27,3 +27,27 @@ reverse = string[::-1]
 print("The string is{} ".format(string))
 print("The reversed string is  {} ".format(reverse))
 
+# ....Palindrome String....
+print("....palindrome sequence....")
+
+"""read a string from user and check whether it is palindrome or not"""
+string  = input("Enter the string")
+rev = string[::-1]
+
+if string==rev:
+    print("The enterted string is palindrome")
+else:
+     print("The entered string is not palindrome")
+     print("............................")
+
+
+# BMI calculator
+print("....BMI CALCULATOR....")
+"""read height and weight from user and calculate the Body Mass Index of the user."""
+print("....BMI calculator....")
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+bmi =  (weight / (height * height))
+print(f"Your body mass index is {bmi}.")
+print("..................")
+
