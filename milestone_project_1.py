f3e573f57e39c50c9a2f55db07a073ab5dc7bc14
jishnu <jishnurajesh555@gmail.com>
2018-08-10 20:50:51 +0530

    vowels_in_a_string

--- milestone_project_1.py
+++ milestone_project_1.py
@@ -1,3 +1,5 @@
+# ....vowels in a String....
+print("...vowels in a string....")
 """Read a string from user and count the number of vowels in the given String"""
 """input : 
 Enter the string : i like apple
@@ -11,5 +13,17 @@ for i in my_string:
     for j in vowels:
         if i==j:
             count+=1
-print(f"The number of vowels in the given String is {count}.")	      
-	      
+print(f"The number of vowels in the given string is {count}.")
+# ....Reverse of a string....
+print("...Reverse of a string....")
+"""Read a string from user and reverse and print the string."""
+"""input:
+Enter the string : i love python
+output :
+The reversed string is nohtyp evol i """
+
+string = input("Enter the string : ")
+reverse = string[::-1]
+print("The string is{} ".format(string))
+print("The reversed string is  {} ".format(reverse))
+
