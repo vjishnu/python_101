6c911fb7926579daee8646523a9a5b7ab0b55fec
jishnu <jishnurajesh555@gmail.com>
2018-08-11 11:36:15 +0530

    update milestone_p_1

--- milestone_project_1.py
+++ milestone_project_1.py
@@ -95,3 +95,18 @@ for i in range(0,num):
  * * * *
 * * * * *  '''
 
+
+# sort a string in alphabetical order
+'''read a string and sort it in alphabetical order.'''
+print("....sort a string in alphabetical order....")
+
+s1=input("Enter the string to sort in alphabetic order :")
+b =''.join(sorted(s1)) # remove space between them using join function.
+print(b)
+'''input:
+Enter the string to sort in alphabetic order : i love python
+output: ehilnooptvy
+'''
+print("...........................")
+
+
