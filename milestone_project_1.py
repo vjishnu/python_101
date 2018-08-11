671d772db86730bca2823d891629ff0c5d1ed40a
jishnu <jishnurajesh555@gmail.com>
2018-08-11 10:48:08 +0530

    update milestone_p_1

--- milestone_project_1.py
+++ milestone_project_1.py
@@ -51,3 +51,30 @@ bmi =  (weight / (height * height))
 print(f"Your body mass index is {bmi}.")
 print("..................")
 
+
+# Factorial of a Number
+"""Read a number from user and find its factorial."""
+print("....Factorial of a number....")
+num = int(input("Enter the number"))
+product = 1
+for i in range(num):
+    product = product * (i+1) # 4! = 4*3*2*1
+print(f"The factorial of the given number is {product}.")
+'''input :
+Enter the number7
+output :
+The factorial of the given number is 5040.'''
+
+print(".......................")
+
+# using math
+
+import math
+from math import factorial
+print("....Factorial of a number using buildin function....")
+num = int(input("Enter the number"))
+fact = math.factorial(num)
+print(f"The factorial of the given number is {fact}.")
+
+print(".......................")
+
