9de94fe8d904a746025241908a83a2fd3a18dd82
jishnu <jishnurajesh555@gmail.com>
2018-08-14 09:22:45 +0530

    additon

--- addition.py
+++ addition.py
@@ -4,4 +4,10 @@ a = int(input("Enter 1st  number : "))
 b = int(input("Enter 2nd number "))
 print("The sum is : ",add(a,b))
 
-
+def cheese_and_crackers(cheese_count,box_of_cheeses):
+    return cheese_count , box_of_cheeses
+cheese_count = int(input("Enter the no. of cheese count")) 
+box_of_cheeses = int(input("Enter the no. of boxes of cheess"))
+cheese_and_crackers(cheese_count,box_of_cheeses)
+print(f"You have {cheese_count} cheeses!")  
+print(f"You have {box_of_cheeses} boxes of crackers!") 
